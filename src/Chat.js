a918ebb47642de784a17ac98c36fa1be7e52c675
Ryan Ardray <rardray@gmail.com>
2019-01-31 14:32:36 -0600

    working on deploy fix

--- src/Chat.js
+++ src/Chat.js
@@ -1,67 +1,68 @@
-import React, { Component } from 'react';
-import io from 'socket.io-client'
-import Input from './Input'
-import Conversation from './Conversation'
+import React, { Component } from "react";
+import io from "socket.io-client";
+import Input from "./Input";
+import Conversation from "./Conversation";
 
 class Chat extends Component {
-    constructor(props) {
-        super(props)
-        this.state ={ messages: [], notice: '' }
-        this.socket = io('localhost:8080')
-        this.socket.on('RECEIVE_MESSAGE', function(data) {
-           getData(addMessage)
-        })
-        const getData = this.getData
-        const addMessage = this.addMessage
-    }
-    getData = ( callback ) => {
-        fetch('http://localhost:8080/messages')
-        .then(res => res.json())
-        .then(messages => callback(messages))
-    }
-    addMessage = (data) => {
-        this.setState( { messages: data } )
-    }
-    componentWillMount() {
-        this.getData(this.addMessage)
-    }
-    handleComment = ( value, value2 ) => {
-    fetch('http://localhost:8080/messages/add', {
-        method: 'POST',
-        body: JSON.stringify({author: value, message: value2}),
-        headers: { "Content-type": "application/json; charset=UTF-8" }
+  constructor(props) {
+    super(props);
+    this.state = { messages: [], notice: "" };
+    this.socket = io("http://localhost:3006");
+    this.socket.on("RECEIVE_MESSAGE", function(data) {
+      getData(addMessage);
+    });
+    const getData = this.getData;
+    const addMessage = this.addMessage;
+  }
+  getData = callback => {
+    fetch("http://localhost:3006/messages")
+      .then(res => res.json())
+      .then(messages => callback(messages));
+  };
+  addMessage = data => {
+    this.setState({ messages: data });
+  };
+  componentWillMount() {
+    this.getData(this.addMessage);
+  }
+  handleComment = (value, value2) => {
+    fetch("http://localhost:3006/messages/add", {
+      method: "POST",
+      body: JSON.stringify({ author: value, message: value2 }),
+      headers: { "Content-type": "application/json; charset=UTF-8" }
     })
-    .then(()=> this.socket.emit('SEND_MESSAGE'))
-    .catch(err => err)
-}
-    render() {
-        return (
-        <div className = 'containter'>
-            <div className = 'row'>
-                <div className = 'col-4'>
-                    <div className = 'card' >
-                        <div className = 'card-body'>
-                            <div className = 'card-title'>Chat</div>
-                                <hr/>
-                                    <div className = 'messages'>
-                                    {this.state.messages.map(el => {
-            return (
-                <Conversation 
-                    value = {el.author} 
-                    value2 = {el.message} 
-                    value3 = {el.author.charAt(0)}
-                    id = {el._id}/>
-            )
-        })}
-                                    </div>
-                                </div>
-                               <Input handleComment = {this.handleComment} />
-                            </div>
-                        </div>
-                    </div>
+      .then(() => this.socket.emit("SEND_MESSAGE"))
+      .catch(err => err);
+  };
+  render() {
+    return (
+      <div className="containter">
+        <div className="row">
+          <div className="col-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-title">Chat</div>
+                <hr />
+                <div className="messages">
+                  {this.state.messages.map(el => {
+                    return (
+                      <Conversation
+                        value={el.author}
+                        value2={el.message}
+                        value3={el.author.charAt(0)}
+                        id={el._id}
+                      />
+                    );
+                  })}
                 </div>
-        )
-    }
+              </div>
+              <Input handleComment={this.handleComment} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Chat;

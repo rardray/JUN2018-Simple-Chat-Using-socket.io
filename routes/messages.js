7da9db496479fd55aacee7bdd844a0de780ea172
Ryan Ardray <rardray@gmail.com>
2019-02-01 13:42:29 -0600

    sdf

--- routes/messages.js
+++ routes/messages.js
@@ -4,9 +4,7 @@ var mongoose = require("mongoose");
 var app = express();
 var socket = require("socket.io");
 
-mongoose.connect(
-  "mongodb://rardray:Dominion1234@ds049624.mlab.com:49624/socket-serv"
-);
+mongoose.connect(process.env.MONGOLAB_URI);
 var db = mongoose.connection;
 db.on("error", function(msg) {
   console.log("Mongoose: Unable to connect ot database");

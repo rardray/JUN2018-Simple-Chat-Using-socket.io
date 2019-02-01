a638991d9625c060707a8084c9e25b6afbf8a7f4
Ryan Ardray <rardray@gmail.com>
2019-01-31 22:25:42 -0600

    sdf

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var indexRouter = require("./routes/index");
 var messagesRouter = require("./routes/messages");
 var app = express();
 var io = require("socket.io").listen(app.listen(process.env.PORT || 3006));
-var cors = require("cors");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -20,8 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json({ extended: true }));
-
-app.use("/", indexRouter);
 
 app.use("/messages", messagesRouter);
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -42,16 +39,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
-});
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
 });
 
 io.sockets.on("connection", socket => {

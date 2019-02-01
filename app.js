e75f2bada727304e6622effa9a3101679469815e
Ryan Ardray <rardray@gmail.com>
2019-01-31 23:20:21 -0600

    wer

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var app = express();
 var socketIO = require("socket.io");
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,7 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-const server = express()
+const server = app
   .use((req, res) => res.sendFile(INDEX))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
